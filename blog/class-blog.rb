e010044ada4156924dd27dde57d65e9ee8c2f9fc
Taylor Daugherty <taylor.l.daugherty@gmail.com>
2016-02-01 18:23:33 -0600

    Practice writing classes

---
+++ blog/class-blog.rb
@@ -0,0 +1,38 @@
+class AdoptACat
+
+  def initialize(breeds, gender, girl_names, boy_names)
+    @breed = breeds
+    @gender = gender
+    @girl_names = girl_names
+    @boy_names = boy_names
+  end
+
+  def breed
+    @breed.sample
+  end
+
+  def gender
+    @gender.sample
+  end
+
+  def name(gender)
+
+    if gender == gender[0]
+      @boy_names.sample
+    else
+      @girl_names.sample
+    end
+  end
+end
+
+
+breeds = ["Himalayan", "Persian", "Russian Blue", "Tabby", "Scottish Fold"]
+gender = ["male", "female"]
+girl_names = ["Chunk", "girl"]
+boy_names = ["Poop", "boy"]
+
+my_cat = AdoptACat.new(breeds, gender, girl_names, boy_names)
+
+puts "You should adopt a #{my_cat.gender} " + "#{my_cat.breed} and name it " + "#{my_cat.name(gender)}."
+
+
